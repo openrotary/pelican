1cdfa58d6655593707fd11b5cd537acfd5b7b07f
songjunjie <songjunjie@ygsoft.com>
2020-01-03 17:14:48 +0800

    格式化

--- pages/CanvasAround/index.vue
+++ pages/CanvasAround/index.vue
@@ -3,13 +3,42 @@
       div.around {
           @dragover: handleDragover
           @drop: handleDrop
+          RenderCard {
+              %: tree
+              :data: $it
+          }
       }
   }
 </template>
 
 <script>
+import RenderCard from './RenderCard'
+import Leaf from '@/packages/leaf-js'
+const leaf = new Leaf()
 export default {
     name: 'CanvasAround',
+    components: {
+        RenderCard
+    },
+    data: () => ({
+        tree: [],
+        renderData: []
+    }),
+    mounted() {
+        this.tree = [
+            {
+                tagName: 'section',
+                isSingle: false,
+                type: 2,
+                children: [
+                    { tagName: 'div', isSingle: false, type: 2, children: [{ type: 1, content: 'JSON解析' }], attr: {} }
+                ],
+                attr: {}
+            }
+        ]
+        this.renderData = Leaf.tree2arr(this.tree)
+        console.log(this.renderData)
+    },
     methods: {
         handleDragover(e) {
             e.preventDefault()
@@ -18,6 +47,8 @@ export default {
         handleDrop(e) {
             console.log('松手', e.clientX, e.clientY)
             // 插入数据
+            // this.tree = leaf.appendNode({ x: e.clientX, y: e.clientY })
+            // console.log(this.tree)
         }
     }
 }
@@ -31,6 +62,7 @@ export default {
         justify-content: center;
 
         .around {
+            padding: 50px;
             min-width: 100%;
             min-height: 100%;
             overflow: auto;
---
+++ pages/CanvasAround/renderCard.vue
@@ -0,0 +1,36 @@
+<template lang="tea">
+section.card {
+    renderCard {
+        ?: data.children
+        %: data.children
+        :data: $it
+    }
+    div {
+        /
+        ~~{{data.tagName}}
+    }
+}
+</template>
+<script>
+export default {
+    name: 'renderCard',
+    props: {
+        data: {
+            type: Object,
+            default: () => {}
+        }
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+    .card {
+        display: flex;
+
+        > div {
+            height: 80px;
+            min-width: 250px;
+            background: #fff;
+        }
+    }
+</style>
