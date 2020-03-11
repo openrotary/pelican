7e1ab8b9a6c7d1de593893266435717d0808c069
songjunjie <songjunjie@ygsoft.com>
2020-03-11 11:04:43 +0800

    修改样式

--- pages/ControlView/EditPage.vue
+++ pages/ControlView/EditPage.vue
@@ -154,101 +154,101 @@ export default {
 }
 </script>
 <style lang="stylus" scoped>
-.edit-page {
-    position: fixed;
-    transition: all 0.4s ease;
-    width: 600px;
-    z-index: 2;
-    border-radius: 10px;
-    box-shadow: 0 0 10px #55295C;
-    background: #fff;
-    top: 100vh;
-    bottom: 0;
-    right: 840px;
-    padding: 40px 0;
-    box-sizing: border-box;
+    .edit-page {
+        position: fixed;
+        transition: all 0.4s ease;
+        width: 600px;
+        z-index: 2;
+        border-radius: 10px;
+        box-shadow: 0 0 10px #55295C;
+        background: #fff;
+        top: 100vh;
+        bottom: 0;
+        right: 840px;
+        padding: 50px 0;
+        box-sizing: border-box;
 
-    &.show {
-        top: 70px;
-        bottom: 15px;
-    }
+        &.show {
+            top: 70px;
+            bottom: 15px;
+        }
 
-    .attr {
-        li {
-            list-style-type: none;
+        .attr {
+            li {
+                list-style-type: none;
 
-            &.inline {
-                padding: 0 20px;
-                display: flex;
-                align-items: center;
-                height: 30px;
-                margin-bottom: 30px;
-            }
+                &.inline {
+                    padding: 0 20px;
+                    display: flex;
+                    align-items: center;
+                    height: 30px;
+                    margin-bottom: 30px;
+                }
 
-            &.block {
-                border-bottom: 1px solid #f1f1f1;
-                padding: 10px 20px;
+                &.block {
+                    border-bottom: 1px solid #f1f1f1;
+                    padding: 10px 20px;
+
+                    .title {
+                        padding: 0;
+                    }
+                }
+
+                >input {
+                    flex: 1;
+                    display: block;
+                    width: 240px;
+                    height: 100%;
+                    border: none;
+                    padding: 0 10px;
+                    border-bottom: 1px solid rgba(85, 41, 91, 0.5);
+                    font-size: 16px;
+                }
+
+                .radio {
+                    width: 200px;
+                    display: flex;
+                    justify-content: space-around;
+                }
 
                 .title {
-                    padding: 0;
+                    color: rgba(85, 41, 91, 0.8);
+                    width: 120px;
+                    height: 30px;
+                    display: flex;
+                    align-items: center;
+                    font-size: 14px;
+                    user-select: none;
+                }
+
+                .css-code {
+                    font-size: 12px;
+                    padding: 10px 20px;
                 }
             }
+        }
 
-            >input {
-                flex: 1;
-                display: block;
-                width: 240px;
-                height: 100%;
-                border: none;
-                padding: 0 10px;
-                border-bottom: 1px solid rgba(85, 41, 91, 0.5);
-                font-size: 16px;
-            }
+        .close {
+            position: absolute;
+            right: 10px;
+            top: 10px;
+            display: block;
+            width: 25px;
+            height: 25px;
+            cursor: pointer;
+            border-radius: 50%;
+            transition: all 0.4s ease;
+            transform: rotate(0deg);
+            background-color: #fff;
+            background-size: 80% 80%;
+            background-position: center center;
+            background-repeat: no-repeat;
+            background-image: url('../../assets/icon-close.svg');
 
-            .radio {
-                width: 200px;
-                display: flex;
-                justify-content: space-around;
-            }
-
-            .title {
-                color: rgba(85, 41, 91, 0.8);
-                width: 120px;
-                height: 30px;
-                display: flex;
-                align-items: center;
-                font-size: 14px;
-                user-select: none;
-            }
-
-            .css-code {
-                font-size: 12px;
-                padding: 10px 20px;
+            &:hover {
+                background-color: rgba(85, 41, 91, 0.2);
+                transform: rotate(-90deg);
             }
         }
     }
-
-    .close {
-        position: absolute;
-        right: 10px;
-        top: 10px;
-        display: block;
-        width: 25px;
-        height: 25px;
-        cursor: pointer;
-        border-radius: 50%;
-        transition: all 0.4s ease;
-        transform: rotate(0deg);
-        background-color: #fff;
-        background-size: 80% 80%;
-        background-position: center center;
-        background-repeat: no-repeat;
-        background-image: url('../../assets/icon-close.svg');
-
-        &:hover {
-            background-color: rgba(85, 41, 91, 0.2);
-            transform: rotate(-90deg);
-        }
-    }
-}
 </style>
